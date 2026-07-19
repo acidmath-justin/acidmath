40f1bec9ea75f27aeef3210c123a8cc5a43a1411
acidmath-justin <justin.c.doyle@gmail.com>
2026-07-19 06:40:44 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { Hero } from "@/components/hero/Hero";
 import { HomeSections } from "@/components/home/HomeSections";
 import { getProducts } from "@/lib/shopify";
